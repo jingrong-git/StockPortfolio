cc9084a8d4bbd8e91602904d6ceca3e7ab4d2624
Jingrong-git <zjr_wys@yahoo.com>
2026-02-18 12:14:12 -0800

    Initial Commit

---
+++ StockPortfolio/StockPortfolioApp.swift
@@ -0,0 +1,21 @@
+//
+//  StockPortfolioApp.swift
+//  StockPortfolio
+//
+//  Created by Jingrong Zhou on 2/18/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct StockPortfolioApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ StockPortfolioTests/StockPortfolioTests.swift
@@ -0,0 +1,16 @@
+//
+//  StockPortfolioTests.swift
+//  StockPortfolioTests
+//
+//  Created by Jingrong Zhou on 2/18/26.
+//
+
+import Testing
+
+struct StockPortfolioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
